d52ff8e1191b95a27b194f7fb1d0c2f2a4a12967
liamsorsby <liam@eezeecommerce.com>
2017-03-31 11:06:57 +0100

    add more tests

--- Tests/CardnetHostedGatewayFactoryTest.php
+++ Tests/CardnetHostedGatewayFactoryTest.php
@@ -21,4 +21,47 @@ class CardnetHostedGatewayFactoryTest extends \PHPUnit_Framework_TestCase
         $rc = new \ReflectionClass("liamsorsby\CardNetHosted\CardnetHostedGatewayFactory");
         $this->assertTrue($rc->implementsInterface("Payum\Core\GatewayFactoryInterface"));
     }
+
+    /**
+     * @test
+     */
+    public function couldBeConstructedWithoutAnyArguments()
+    {
+        new CardnetHostedGatewayFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateCoreGatewayFactoryIfNotPassed()
+    {
+        $factory = new CardnetHostedGatewayFactory();
+        $this->assertAttributeInstanceOf('Payum\Core\CoreGatewayFactory', 'coreGatewayFactory', $factory);
+    }
+
+
+    /**
+     * @test
+     */
+    public function shouldUseCoreGatewayFactoryPassedAsSecondArgument()
+    {
+        $coreGatewayFactory = $this->getMock('Payum\Core\GatewayFactoryInterface');
+        $factory = new CardnetHostedGatewayFactory(array(), $coreGatewayFactory);
+        $this->assertAttributeSame($coreGatewayFactory, 'coreGatewayFactory', $factory);
+    }
+
+
+    /**
+     * @test
+     */
+    public function shouldAllowCreateGateway()
+    {
+        $factory = new CardnetHostedGatewayFactory();
+        $gateway = $factory->create(array('publishable_key' => 'aPubKey', 'secret_key' => 'aSecretKey'));
+        $this->assertInstanceOf('Payum\Core\Gateway', $gateway);
+        $this->assertAttributeNotEmpty('apis', $gateway);
+        $this->assertAttributeNotEmpty('actions', $gateway);
+        $extensions = $this->readAttribute($gateway, 'extensions');
+        $this->assertAttributeNotEmpty('extensions', $extensions);
+    }
 }
