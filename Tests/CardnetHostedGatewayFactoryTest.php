435e58c81e15171038419d12f3607bf215247cc5
liamsorsby <liam@eezeecommerce.com>
2017-04-25 10:27:31 +0100

    add api to test

--- Tests/CardnetHostedGatewayFactoryTest.php
+++ Tests/CardnetHostedGatewayFactoryTest.php
@@ -8,6 +8,7 @@
 
 namespace liamsorsby\CardNetHosted\Tests;
 
+use liamsorsby\CardNetHosted\Api;
 use liamsorsby\CardNetHosted\CardnetHostedGatewayFactory;
 
 class CardnetHostedGatewayFactoryTest extends \PHPUnit_Framework_TestCase
@@ -59,6 +60,7 @@ class CardnetHostedGatewayFactoryTest extends \PHPUnit_Framework_TestCase
     {
         $factory = new CardnetHostedGatewayFactory();
         $gateway = $factory->create(array('sandbox' => true, 'txntype' => 'sale', 'storename' => '1111', 'mode' => 'payonly', "shared_secret" => "123", "password" => "password"));
+        $gateway->addApi(Api::class);
         $this->assertInstanceOf('Payum\Core\Gateway', $gateway);
         $this->assertAttributeNotEmpty('apis', $gateway);
         $this->assertAttributeNotEmpty('actions', $gateway);
